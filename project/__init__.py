aaffec818a8b4ad731c30f1c926ffa75f6db8dc3
TiannaDeSpain <des17015@byui.edu>
2021-11-19 14:50:08 -0700

    added __init__ classes

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,45 @@
 """
 The root package contains all the source code for the game.
 """
+from game import constants
+import arcade
+
+
+class Game(arcade.window): 
+    
+    def setup(self): 
+        pass
+
+    def on_draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def restart(self):
+        pass
+    
+    def draw_game_over(self):
+        pass
+
+    def draw_score(self): 
+        pass
+    
+    def check_keys(self):
+        pass
+    
+    def on_key_press(self):
+        pass
+    
+    def on_key_release(self): 
+        pass
+    
+    def check_collisions(self):
+        pass
+    
+    def cleanup(self):
+        pass
+    
+    def create_enemys(self): 
+        pass
+    
--- project/__main__.py
+++ project/__main__.py
@@ -2,9 +2,11 @@ import arcade
 from game import constants
 from game.actor import Actor
 from game.point import Point
+from game.__init__ import Game
 
 def main(screen):
-    pass
+    arcade.Window = Game(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
+    arcade.run()
 
 if __name__ == "__main__":
     main()
