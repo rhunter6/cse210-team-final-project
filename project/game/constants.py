6d5cec27e86f55b19468f48b599b9424a1d955b7
TiannaDeSpain <des17015@byui.edu>
2021-11-19 15:15:29 -0700

    removed indivdual class files

--- project/game/constants.py
+++
@@ -1,7 +0,0 @@
-import arcade
-
-SCREEN_TITLE = "tobedetermined"
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600 
-
-
--- project/game/velocity.py
+++
@@ -1,4 +0,0 @@
-class Velocity():
-    def __init__(self):
-        self.dx = 1.0
-        self.dy = 1.0
