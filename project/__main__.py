2399e78395ed52a3f91a004728309c305c3ad4a1
nephi-byui <byui@nephi.malit.me>
2021-11-21 01:22:31 +0800

    fixed

--- project/__main__.py
+++ project/__main__.py
@@ -2,9 +2,6 @@ import arcade
 from game.director import Director
 from game import constants
 
-def main(screen):
-    pass
-
 def main():
     """ Main function """
     window = Director(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
