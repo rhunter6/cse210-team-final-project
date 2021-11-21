fe9f92ebc6ece6430d8241f77aaa57802b85a902
Mario1355 <91709155+Mario1355@users.noreply.github.com>
2021-11-21 15:49:30 -0700

    Killer Chess Randomized Boared and moving piece

---
+++ killerchess/game/director.py
@@ -0,0 +1,17 @@
+from game.explodingcheckers import ExplodingCheckers
+import constants
+import arcade
+
+class Director:
+    
+    def __init__(self):
+        """
+        """
+        self._keep_playing = True
+        self.cast = {}
+        
+        
+    def StartGame(self):
+        ExplodingCheckers(constants.MaxX,constants.MaxY,constants.ScreenTitle)
+        
+            
---
+++ killerchess/game/tiles.py
@@ -0,0 +1,9 @@
+import arcade
+import random
+import Assets
+
+class tile(arcade.Sprite):
+    
+    def __init__(self,x,y):
+        self.center_x = x
+        self.center_y = y
