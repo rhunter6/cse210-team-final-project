279ea576fab4065d275f77422b17e56105eb4fb6
keviner99 <esp21007@byui.edu>
2021-11-21 00:43:23 -0500

    Try to put some code in move.py

--- project/game/move.py
+++ project/game/move.py
@@ -1,1 +1,10 @@
+import arcade
 
+class Move(arcade.Window) :
+    
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        
+        
+    pass
+    
