1193573b60554e28b757ff651eaa6eec5789662a
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-11-27 17:34:13 -0700

    adding all ben-branch files to kc branch so I can work with them

---
+++ project/game/director.py
@@ -0,0 +1,15 @@
+from game.explodingcheckers import ExplodingCheckers
+import constants
+import arcade
+
+class Director:
+
+    def __init__(self):
+        """
+        """
+        self._keep_playing = True
+        self.cast = {}
+
+
+    def StartGame(self):
+        ExplodingCheckers(constants.MaxX,constants.MaxY,constants.ScreenTitle)
