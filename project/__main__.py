08f252dc900a6511f7bcdec734eb1ea37870efde
Tianna DeSpain <u663140950@byui.edu>
2021-12-11 09:59:31 -0700

    added function comments *still need level_01 comt*

--- project/__main__.py
+++ project/__main__.py
@@ -3,10 +3,16 @@ from game.director import Director
 from game import constants
 
 def main():
-    """ Main function """
+    """ Main function 
+    """
     window = Director(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+
+    #set up game enviroment 
     window.setup()
+
+    #runs game
     arcade.run()
+
 
 if __name__ == "__main__":
     main()   
